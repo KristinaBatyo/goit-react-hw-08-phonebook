42d53b9a726d0715d9c5b72890b5bd6b49d22fb5
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2023-04-23 23:39:55 +0300

    local

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,22 @@ import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contacts';
 import { filtersReducer } from './filters';
 import authReducer from './auth/auth-slice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['token'],
+};
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
     contacts: contactsReducer,
     filters: filtersReducer,
   },
 });
+
+export const persistor = persistStore(store);
